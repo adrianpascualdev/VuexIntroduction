86541e7ffcb167c51a64f766402103ca01b02274
linux <adrianpascualdev@gmail.com>
2023-07-12 18:04:27 +0200

    Implemented changes

--- src/helpers/getRandomInt.js
+++ src/helpers/getRandomInt.js
@@ -3,7 +3,7 @@
 const getRandomInt = () => {
 
     return new Promise( resolve => {    
-        const rndInt = (Math.random() * 20) + 1
+        const rndInt = Math.floor ((Math.random() * 20) + 1)
 
         setTimeout(() => {
             resolve(rndInt)
